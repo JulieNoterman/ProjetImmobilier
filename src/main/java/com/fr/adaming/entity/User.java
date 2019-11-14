4136838872b042890f0428a87f1dde372603f967
corneloup theo <theo.corneloup42@gmail.com>
2019-11-14 12:04:48 +0100

    Finalisation abstract User

--- src/main/java/com/fr/adaming/entity/User.java
+++ src/main/java/com/fr/adaming/entity/User.java
@@ -3,7 +3,11 @@ package com.fr.adaming.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.validation.constraints.Pattern;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +15,11 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity @Getter @Setter @NoArgsConstructor @ToString
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class User {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	@Column(unique = true, nullable = false)
@@ -22,4 +27,7 @@ public abstract class User {
 	
 	@Column(nullable = false)
 	private String fullName;
+	
+	@Pattern(regexp = "\\d{10}")
+	private Long telephone;
 }
