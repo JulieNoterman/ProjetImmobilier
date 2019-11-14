8572b26266d768d74fed069a85b22a412d9ccc79
DylanSalos <dylan.salos@yahoo.com>
2019-11-14 12:19:06 +0100

    Ajouter des classes bien

---
+++ src/main/java/com/fr/adaming/service/impl/BienServiceImpl.java
@@ -0,0 +1,8 @@
+package com.fr.adaming.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BienServiceImpl {
+
+}
---
+++ src/main/java/com/fr/adaming/web/controller/IBienController.java
@@ -0,0 +1,8 @@
+package com.fr.adaming.web.controller;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IBienController {
+
+}
