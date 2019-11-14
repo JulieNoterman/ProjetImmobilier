dee7e2f76fb0607d116cecde89d261fc7146ef32
DylanSalos <dylan.salos@yahoo.com>
2019-11-14 12:44:44 +0100

    Modif des classes Bien 3.0

--- src/main/java/com/fr/adaming/web/controller/impl/BienControllerImpl.java
+++ src/main/java/com/fr/adaming/web/controller/impl/BienControllerImpl.java
@@ -1,10 +1,16 @@
 package com.fr.adaming.web.controller.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fr.adaming.service.IBienService;
 import com.fr.adaming.web.controller.IBienController;
 
 @RestController
 public class BienControllerImpl implements IBienController {
 
+	@Autowired
+	@Qualifier ("BienServiceImpl")
+	private IBienService service;
 }
