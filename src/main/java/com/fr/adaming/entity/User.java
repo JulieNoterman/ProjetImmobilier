4aaa93ef7ec23f477a72c90de9b878b93a7308df
JulieNoterman <julie.noterman@gmail.com>
2019-11-14 11:44:44 +0100

    Ajouter l'attribut email dans USer.java

--- src/main/java/com/fr/adaming/entity/User.java
+++ src/main/java/com/fr/adaming/entity/User.java
@@ -1,13 +1,22 @@
 package com.fr.adaming.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity @Getter @Setter @NoArgsConstructor @ToString
 public class User {
 
 	@Id
 	@GeneratedValue
 	private Long id;
+	
+	@Column(unique = true, nullable = false)
+	private String email;
 }
