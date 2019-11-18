62510f8f9e1b6d9b1ceac6d72a7b2b641e9f49d4
DylanSalos <dylan.salos@yahoo.com>
2019-11-18 15:34:44 +0100

    ajout Log4j

--- src/main/java/com/fr/adaming/ProjetImmobilierApplication.java
+++ src/main/java/com/fr/adaming/ProjetImmobilierApplication.java
@@ -1,14 +1,21 @@
 package com.fr.adaming;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetImmobilierApplication {
 
+	private final static Logger logger = LogManager.getLogger(ProjetImmobilierApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetImmobilierApplication.class, args);
 		
+		logger.warn("coucou");
+		logger.error("BOOOOOOOOOOOB");
+		logger.debug("BOOOOOOOOOOOB2");
 	}
 
 }
