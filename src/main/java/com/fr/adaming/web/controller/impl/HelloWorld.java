072b5a1565b7943529cdd6479a7526972f768070
DylanSalos <dylan.salos@yahoo.com>
2019-11-18 16:48:53 +0100

    json to java et vice_versa

--- src/main/java/com/fr/adaming/web/controller/impl/HelloWorld.java
+++ src/main/java/com/fr/adaming/web/controller/impl/HelloWorld.java
@@ -1,16 +1,35 @@
 package com.fr.adaming.web.controller.impl;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fr.adaming.service.IClientService;
+import com.fr.adaming.web.dto.ClientSaveDto;
+import com.fr.adaming.web.dto.converter.ClientDtoConverter;
 
 @RestController
 @RequestMapping(path = "api/hello")
 public class HelloWorld {
 	
 
+	@Autowired
+	private IClientService service;
+	
 	@GetMapping
 	public String SayHello() {
 		return "Coucou";
 	}
+	
+	@PostMapping
+	public ClientSaveDto save(@Valid @RequestBody ClientSaveDto dto) {
+		return ClientDtoConverter.convertToDto(service.save( ClientDtoConverter.convertToDto(dto)));   
+		
+	}
+	
 }
--- src/test/java/com/fr/adaming/web/controller/impl/HelloWorldControllerTest.java
+++ src/test/java/com/fr/adaming/web/controller/impl/HelloWorldControllerTest.java
@@ -1,6 +1,7 @@
 package com.fr.adaming.web.controller.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,7 +17,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fr.adaming.enumeration.TypeClient;
+import com.fr.adaming.web.dto.ClientSaveDto;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,17 +30,44 @@ public class HelloWorldControllerTest {
 	@Autowired
 	private MockMvc mvc;
 	
-	private ObjectMapper mapper;
+	private ObjectMapper mapper = new ObjectMapper();
+	
+	
 	
 	@Test
 	public void sayHello_ShouldReturnHelloWorld() throws UnsupportedEncodingException, Exception {
+		
+		
 		
 		
 		String mvcresult = mvc.perform(get("/api/hello")
 		.contentType(MediaType.APPLICATION_JSON))
 		.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
 		
+		
+		
+		
 		assertEquals("Coucou", mvcresult);
+		
+		
+	}
+	
+	@Test
+	public void testMockito_ShouldReturnHelloWorld() throws UnsupportedEncodingException, Exception {
+		
+		ClientSaveDto client = new ClientSaveDto("a@a.fr","aaaa",TypeClient.ACHETEUR,12345678L);
+		
+		
+		String mvcresult = mvc.perform(get("/api/hello")
+		.contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(client)))
+		.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+		
+		ClientSaveDto dtoResult = mapper.readValue(mvcresult, ClientSaveDto.class);
+		
+		assertNotNull(dtoResult);
+		assertEquals("a@a.fr", client.getEmail());
+		assertEquals("aaaa", client.getFullname());
+		
 		
 		
 	}
